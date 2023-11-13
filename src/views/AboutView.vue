e64d7aa94f1d308ca53e90ddd74e6d791370cfa9
anutka03 <akotelnikova@diferenteweb.com>
2023-11-13 22:14:33 +0200

    init commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>A Site For events To Better The World</h1>
+  </div>
+</template>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
